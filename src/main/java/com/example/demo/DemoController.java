002823a2d38e39810363ecfa0175e6cb68d30c74
Jordan Gelling <gelljord@gmail.com>
2022-08-24 23:02:09 +1200

    Adding basic navigation

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,7 +3,8 @@ package com.example.demo;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 @Controller
 public class DemoController {
 
@@ -15,8 +16,14 @@ public class DemoController {
     }
 
 
-    @GetMapping("/orders")
-    String getOrders(){
-        return "orders";
+    @PostMapping(value="/index")
+    String getOrders(String Name, String Address){
+        System.out.println(Name);
+        System.out.println(Address);
+        if(Address != "" && Name != ""){
+            return "orders";
+        } else {
+            return "index";
+        }
     }
 }
