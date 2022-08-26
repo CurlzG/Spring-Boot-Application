ced91447a86b40d53b23e6a7849cd4c33b8433e3
Jordan Gelling <gelljord@gmail.com>
2022-08-27 01:02:33 +1200

    Updating HTML and adding Popup HTML and CSS

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,8 +3,6 @@ package com.example.demo;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
 @Controller
 public class DemoController {
 
@@ -14,15 +12,22 @@ public class DemoController {
 
         return "index";
     }
+    @RequestMapping(value="/popup")
+    String getPopup(){
+        //System.out.println("HELLO");
+
+        return "popup";
+    }
 
 
     @PostMapping(value="/index")
-    String getOrders(String Name, String Address){
+    String getOrders(Model mod, String Name, String Address){
         System.out.println(Name);
         System.out.println(Address);
         if(Address != "" && Name != ""){
             return "orders";
         } else {
+            mod.addAttribute("Invalid", "No Name or Address Added");
             return "index";
         }
     }
